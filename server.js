22761858af0b06279dc2908a4b04a2e0d895becd
Vishal Patel <vishal@homepass.com>
2017-05-15 04:35:44 +1000

    learning graphql

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
+const app = express();
+
+app.use('/graphql', expressGraphQL({
+  schema: schema,
+  graphiql:true
+}));
+
+app.listen ( 4000 , () => {
+  console.log('Listning');
+})
